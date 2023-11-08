4fea0f2fe74b7997ca583a9ede82862504296e2c
Sergio Silva <seu-email@example.com>
2023-11-08 18:02:47 -0300

    Att ex002

--- PyEx/Ex002.py
+++ PyEx/Ex002.py
@@ -1,2 +1,8 @@
-n = input('Digite seu nome: ')
-print(f'\033[1mPrazer em te conhecer, \033[32m{n}\033[m!')
+n = input('\033[1;35mDigite seu nome:\033[m ')
+v1 = False
+if n == '':
+    v1 = True
+if n.isnumeric() or n.isspace()  or  v1  == True:
+    print('\033[1;31m ERROR \033[m \033[33;1m(Você não pode deixar em branco, e também não pode conter só números!) \033[m')
+else:
+    print(f'\033[1;36mSeja muito bem vindo ao meu sistema\033[m,\033[32;1m{n}\033[m!')
