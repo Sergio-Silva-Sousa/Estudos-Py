9af9c7e9178819a235d0447ce80e54840c335ce3
Sergio Silva <seu-email@example.com>
2023-11-10 22:09:12 -0300

    att ex005

--- PyEx/ex005.py
+++ PyEx/ex005.py
@@ -1,2 +1,7 @@
-num = int(input("Digite um número: "))
-print(f' Analisando seu número: \n Seu sucessor é {num + 1} \n Seu antecessor é {num - 1}')
+num = input('\033[34;1mDigite um número:\033[33;1m ')
+if not num.isdigit():
+    print('\033[31;1mERROR\033[33;1m(So posso identificar números inteiros)\033[m')
+else:
+    num = int(num)
+    print(f'\033[36;1mSucessor:\033[32;1m {num + 1}')
+    print(f'\033[36;1mAntecessor:\033[32;1m {num -1}\033[m')
