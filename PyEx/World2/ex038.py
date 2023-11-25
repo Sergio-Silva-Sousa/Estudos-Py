559b56e3f6258755e43757d241827e3168c44d10
Sergio Silva <seu-email@example.com>
2023-11-25 16:59:50 -0300

    add ex038

---
+++ PyEx/World2/ex038.py
@@ -0,0 +1,8 @@
+n1 = int(input('n1: '))
+n2 = int(input('n2: '))
+if n1 > n2:
+      print(fz'{n1} é maior')
+elif n1 < n2:
+      print(f'{n2} é maior')
+elif n1 == n2:
+      print('Não existe valor maior, os dois são iguais')
