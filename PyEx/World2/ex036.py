42095416c0b36fed390f55ee0ad694097a42ce97
Sergio Silva <seu-email@example.com>
2023-11-25 16:55:41 -0300

    ajustes nos exs

--- PyEx/World2/ex036.py
+++ PyEx/World2/ex036.py
@@ -1,8 +1,10 @@
-n1 = int(input('n1: '))
-n2 = int(input('n2: '))
-if n1 > n2:
-      print(f'{n1} é maior')
-elif n1 < n2:
-      print(f'{n2} é maior')
-elif n1 == n2:
-      print('Não existe valor maior, os dois são iguais')
+vlr_cs = float(input('Qual o valor da casa? '))
+slr = float(input('Quanto você recebe? '))
+ans = int(input('Em quantos anos você irá pagar?')) * 12
+p = (slr * 30) / 100
+pr_ms = vlr_cs / ans
+if pr_ms > p:
+    print('Você iria fica um pouco apertado,  assim inprestimo negado')
+else:
+    print(f'Você ira pagar R${pr_ms:.2f} mensalmente, por {ans} meses. ')
+
